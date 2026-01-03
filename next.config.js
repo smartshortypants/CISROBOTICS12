3f42e251b2d8878b8b600e4a7d95590872c83994
smartshortypants <shintassovsaken@gmail.com>
2026-01-04 03:52:37 +0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** Minimal Next.js config */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // If you host the frontend under a subpath (project page), set `basePath` and `assetPrefix`.
+  // assetPrefix: process.env.ASSET_PREFIX || '',
+  // basePath: process.env.BASE_PATH || ''
+}
+
+module.exports = nextConfig
